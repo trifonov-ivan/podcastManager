e775b913836e0599f7e4459e0f682fdabf91d101
ant1kvar <spaeciallyforyou@gmail.com>
2013-03-08 14:32:37 +0300

    initial commit

---
+++ PodcastManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PodcastManager
+//
+//  Created by Ivan Trifonov on 08.03.13.
+//  Copyright (c) 2013 Ivan Trifonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PodcastManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PodcastManager
+//
+//  Created by Ivan Trifonov on 08.03.13.
+//  Copyright (c) 2013 Ivan Trifonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
